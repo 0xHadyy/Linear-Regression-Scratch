fab1df363327b5d2940b42dd7d13732f8ae2e7c2
hady <abdelhadinodejs@gmail.com>
2025-07-11 05:00:03 +0100

    F-statistic, RSE, R_Squared, SE for new response added

--- Linear_Regression_Implementation.py
+++ Linear_Regression_Implementation.py
@@ -1,8 +1,11 @@
 import numpy as np
+import math
+
+from numpy.linalg import LinAlgError
 
 
 # Generating dummy data
-def generate_dummy_data(n=10, p=3, noise_std=1.0, seed=420):
+def generate_dummy_data(n=100, p=3, noise_std=1.0, seed=420):
     np.random.seed(seed)
 
     X_raw = np.random.randn(
@@ -31,6 +34,12 @@ def ols_estimate(X, Y):
         np.linalg.inv(gram_matrix) @ X_transpose @ Y
     )  # beta matrix closed form OLS is (X^TX)^-1X^TY
     return beta_hat, gram_matrix  # estimated beta
+
+
+def predict(X, beta_hat):
+    # Computer the predicted response y_hat
+    y_hat = X @ beta_hat
+    return y_hat
 
 
 # Sample standard deviation Which is the Estimate of the standard deviaion sigma^2
@@ -103,25 +112,85 @@ def confidence_interval_beta(beta_hat, standard_error, df):
     return confidence_interval
 
 
-def f_statistic():
+def f_statistic(RSS, y, p, n):
+    # Compute the f-statistic, testing null hypothesis -> there is no relationship between the predictors and response
+    y_mean = np.mean(y)
+    TSS = np.sum((y - y_mean) ** 2)
+    if p == 0:
+        raise ValueError("invalid Features number")
+    numerator = (TSS - RSS) / p
+    denominator = RSS / (n - p - 1)
+    f_test = numerator / denominator
+
+    return f_test, TSS
+
+
+def residual_standard_error(RSS, p, n):
+    # Computer RSE -> Describe variability left unexplained by the regerssion model
+    if n - p - 1 <= 0:
+        raise ValueError("Degrees of freedom <= 0 Check n and p")
+    RSE = np.sqrt(RSS / (n - p - 1))
+    return RSE
+
+
+def r_squared(RSS, TSS):
+    # Compute R_squared which is the measure of fit and linear relationship between X <-> Y
+    r_squared = 1 - (RSS / TSS)
+    if not (0 <= r_squared <= 1):
+        raise ValueError("Error happened while computing R_sqaured")
+    return r_squared
+
+
+def standard_error_mean_reponse(X0, MSE, gram_matrix):
+    # compute the variance and standard error in a new observed response Y0
+    p = X0.shape[0]
+    if gram_matrix != (p, p):
+        raise ValueError(f"Gram matrix shape {gram_matrix.shape}")
+    if MSE < 0:
+        raise ValueError("MSE is zero or negative")
+    try:
+        inv_gram_matrix = np.linalg.inv(gram_matrix)
+    except np.linalg.LinAlgError:
+        raise ValueError("Gram matrix is not invertible")
+    X0_transpose = X0.T
+    var_Y0 = MSE * (
+        X0_transpose @ inv_gram_matrix @ X0
+    )  # the formula var(Y_0)=MSE*X_0^T(Gram matrix)^-1X_0
+    se_Y0 = np.sqrt(var_Y0)
+    return var_Y0, se_Y0
+
+
+def confidence_interval_response(y, y_hat, standard_error, df):
+    pass
+
+
+def prediction_interval_response():
     pass
 
 
 X, y, beta_true, noise = generate_dummy_data()
-
+n = len(y)
+p = X.shape[1]
 beta_hat, gram_matrix = ols_estimate(X, y)
 y_hat = X @ beta_hat
-mse, rss = mean_squared_error(y, y_hat, p=X.shape[1])
-beta_hat_var, beta_hat_se = standard_error_beta(mse, gram_matrix)
-
+MSE, RSS = mean_squared_error(y, y_hat, p)
+beta_hat_var, beta_hat_se = standard_error_beta(MSE, gram_matrix)
+confidence_interval = confidence_interval_beta(beta_hat, beta_hat_se, 3)
+f_score, TSS = f_statistic(RSS, y, p, n)
+RSE = residual_standard_error(RSS, p, n)
+r2 = r_squared(RSS, TSS)
 
 print("X shape:", X.shape)
 print("y shape:", y.shape)
+print("this is response y", y)
 print("True Beta (Coefficients)", beta_true)
 print("Estimated beta (Coefficients) are :", beta_hat)
 print("y_hat:", y_hat.shape)
-print("Residual squared sum =", round(rss, 4))
-print("Mean squared error= ", round(mse, 4))
+print("Residual squared sum =", round(RSS, 4))
+print("Mean squared error= ", round(MSE, 4))
 print("The Gram matrix: \n", gram_matrix)
 print("beta hat variance :\n", beta_hat_var)
 print("beta hat standard error :\n", beta_hat_se)
+print("The confidence interval for the coefficients is  :\n", confidence_interval)
+print("The residual standard error (Quality of fit) is :", RSE)
+print("The R-Squared for this model  is :", r2)
